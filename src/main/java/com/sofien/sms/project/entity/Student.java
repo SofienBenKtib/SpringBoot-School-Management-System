1754c0ca8d0097f3dfddcbf461ba9521db8b079f
Sofien Ben Ktib <63020241+SofienBenKtib@users.noreply.github.com>
2023-09-07 02:03:18 +0100

    Entities added

---
+++ src/main/java/com/sofien/sms/project/entity/Student.java
@@ -0,0 +1,24 @@
+package com.sofien.sms.project.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Student")
+public class Student {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String fullName;
+    private String email;
+    private String phoneNumber;
+    private String currentGrade;
+}
