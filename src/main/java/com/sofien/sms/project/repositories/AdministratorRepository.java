0498758f23038e69f744cb6bb214fabc38989e9f
Sofien Ben Ktib <63020241+SofienBenKtib@users.noreply.github.com>
2023-09-07 02:10:37 +0100

    Entities added

---
+++ src/main/java/com/sofien/sms/project/repositories/AdministratorRepository.java
@@ -0,0 +1,7 @@
+package com.sofien.sms.project.repositories;
+
+import com.sofien.sms.project.entity.Administrator;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdministratorRepository extends JpaRepository<Administrator,Integer> {
+}
---
+++ src/main/java/com/sofien/sms/project/repositories/StudentRepository.java
@@ -0,0 +1,7 @@
+package com.sofien.sms.project.repositories;
+
+import com.sofien.sms.project.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student,Integer> {
+}
